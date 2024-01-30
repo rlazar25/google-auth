7ff4c932f19e4c78357892f8bcd45a09aa46a11d
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-01-30 19:14:32 +0100

    dodat sign in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { signInWithGoogle } from "./firebase-config"
 
 function App() {
   return (
     <div className="App">
+      <button onClick={signInWithGoogle} > Sign In With Google </button>
 
+      <h1> {localStorage.getItem("name")} </h1>
+      <h1> {localStorage.getItem("email")} </h1>
+      <img src={localStorage.getItem("profilePic")} alt="T" />
     </div>
   );
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +16,22 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+        const name = result.user.displayName;
+        const email = result.user.email;
+        const profilePic = result.user.photoUrl
+
+        localStorage.setItem("name", name);
+        localStorage.setItem("email", email);
+        localStorage.setItem("profilePic", profilePic);
+
+    }).catch((error) => {
+        console.log(error)
+    })
+}
